17c4c24ebb66b786ac2bfbd981b8aa68dce987c6
mashru020 <mashrurrahman16@gmail.com>
2023-12-05 02:51:03 +0600

    first commit

---
+++ src/main/java/mashru/busmanagement/BusManagement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mashru.busmanagement;
+
+/**
+ *
+ * @author mashru
+ */
+public class BusManagement {
+
+    public static void main(String[] args) {
+        MainScreen mr = new MainScreen();
+        
+        mr.setLocationRelativeTo(null);
+        mr.setVisible(true);
+    }
+}
